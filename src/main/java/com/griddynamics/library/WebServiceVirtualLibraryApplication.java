7118aa3a12e8f6118b3e93463771b5f69475cdce
oliashenko <liashen.olga@gmail.com>
2019-05-31 16:58:59 +0200

    Changes after code review

--- src/main/java/com/griddynamics/library/WebServiceVirtualLibraryApplication.java
+++ src/main/java/com/griddynamics/library/WebServiceVirtualLibraryApplication.java
@@ -11,3 +11,13 @@ public class WebServiceVirtualLibraryApplication {
 	}
 
 }
+
+
+//Create a web service for a virtual library app.
+//Library has a set of digital books. Each book has a name, authors name, publication date and a list of genres.
+//There can be several users registered.
+//User can own access to several books. For each order a date is saved, as well as a percentage of the book that
+// the user read (this value can be updated when user reads more pages).
+//There should be an option to "gift" your book to another user. The book will be removed from your library
+// and added to other users library.
+//For each user you should be able to get the list of the genres he reads the most.
